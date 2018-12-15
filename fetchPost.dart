976931fc9075a64b18b0bd5eced5f7176ac60d2d
Gökhan Gerdan <gokhang1327@gmail.com>
2018-12-15 14:23:52 +0300

    Create fetchPost.dart

---
+++ fetchPost.dart
@@ -0,0 +1,18 @@
+// Conver the http.Response to a Post
+
+import 'dart:async';
+
+import 'package:http/http.dart' as http;
+
+Future<Post> fetchPost() async {
+  final response =
+      await http.get('https://jsonplaceholder.typicode.com/posts/1');
+
+  if (response.statusCode == 200) {
+    // If server returns an OK response, parse the JSON
+    return Post.fromJson(json.decode(response.body));
+  } else {
+    // If that response was not OK, throw an error.
+    throw Exception('Failed to load post');
+  }
+}
